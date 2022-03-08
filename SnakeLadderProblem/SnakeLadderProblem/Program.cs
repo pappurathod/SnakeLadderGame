64baf1b92b53977e82687204c563a7a834a497cb
pappurathod <prathod4545@gmail.com>
2022-03-08 16:37:10 +0530

    start 0th position with single player

---
+++ SnakeLadderProblem/SnakeLadderProblem/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Welcome to Snake And Ladder Game");
+namespace SnakeLadderProblem
+{
+    class SnakeLadder
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake And Ladder Game");
+
+            int Start_POSITION = 0;
+            int NUMBER_OF_PLAYER = 1;
+      
+        }
+    }
+}
